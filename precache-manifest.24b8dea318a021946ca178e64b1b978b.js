65a0a90587a728a10ad0a71cd75c1359113467e3
SheMortal <mortalshe@gmail.com>
2020-08-14 21:31:51 +0200

    Updates

---
+++ precache-manifest.24b8dea318a021946ca178e64b1b978b.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa6260f9c444d89e61f6009d377a69c2",
+    "url": "/Transylvenia/index.html"
+  },
+  {
+    "revision": "881e6324d3a9d8501bcc",
+    "url": "/Transylvenia/static/css/main.3dd3e725.chunk.css"
+  },
+  {
+    "revision": "c6d8ec7b97fe4d739e25",
+    "url": "/Transylvenia/static/js/2.901ab7ef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Transylvenia/static/js/2.901ab7ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "881e6324d3a9d8501bcc",
+    "url": "/Transylvenia/static/js/main.38486674.chunk.js"
+  },
+  {
+    "revision": "e0fe8a335653fea37f65",
+    "url": "/Transylvenia/static/js/runtime-main.5dc5aa85.js"
+  },
+  {
+    "revision": "2394abd124eddad5245b6d7ef65521ab",
+    "url": "/Transylvenia/static/media/bg.2394abd1.jpg"
+  },
+  {
+    "revision": "fdec0371b9ecc69ab9551ee180de4184",
+    "url": "/Transylvenia/static/media/details-2.fdec0371.jpeg"
+  },
+  {
+    "revision": "e3df4b6eeac3a315aa031c233f1501fb",
+    "url": "/Transylvenia/static/media/details-3.e3df4b6e.jpeg"
+  },
+  {
+    "revision": "31d3c23fbb09742be31aa80a2f69afb3",
+    "url": "/Transylvenia/static/media/details-4.31d3c23f.jpeg"
+  },
+  {
+    "revision": "352d4f2b492e74f1d06e9227b05b85f4",
+    "url": "/Transylvenia/static/media/hotel-transylvania-poster.352d4f2b.jpg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/Transylvenia/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "88a79d19cb8a353d6a3de8391750646f",
+    "url": "/Transylvenia/static/media/logo.svg.88a79d19.jpeg"
+  },
+  {
+    "revision": "c4137773bcf5fbfda259318ffe914513",
+    "url": "/Transylvenia/static/media/room-1.c4137773.jpg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/Transylvenia/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "47b2bf9f588d0274fe32a0fa4ab393bc",
+    "url": "/Transylvenia/static/media/room-12.47b2bf9f.jpg"
+  },
+  {
+    "revision": "034326e80a2dc3eef565ca6bf187a05e",
+    "url": "/Transylvenia/static/media/room-13.034326e8.jpg"
+  },
+  {
+    "revision": "c58fe3573a3b5198ab7005d5d55565d4",
+    "url": "/Transylvenia/static/media/room-19.c58fe357.jpg"
+  },
+  {
+    "revision": "9cd48593cf94dc043fafd381d738352b",
+    "url": "/Transylvenia/static/media/room-2.9cd48593.jpg"
+  },
+  {
+    "revision": "12e1016b46d89c8101ef46efe126975d",
+    "url": "/Transylvenia/static/media/room-3.12e1016b.jpg"
+  },
+  {
+    "revision": "69d9adc271317527a0cbc7eb72f0be58",
+    "url": "/Transylvenia/static/media/room-4.69d9adc2.jpg"
+  },
+  {
+    "revision": "a3ddeda0bf96c32eaded5270561f4b4c",
+    "url": "/Transylvenia/static/media/room-5.a3ddeda0.png"
+  },
+  {
+    "revision": "f2e858f051e517435121723307b102f7",
+    "url": "/Transylvenia/static/media/room-6.f2e858f0.png"
+  },
+  {
+    "revision": "c32cd34d776a7b3728d1874514ec2cc2",
+    "url": "/Transylvenia/static/media/room-7.c32cd34d.jpg"
+  },
+  {
+    "revision": "7aebf0c1f5b94ad3590f18c4f9ba468b",
+    "url": "/Transylvenia/static/media/room-8.7aebf0c1.jpeg"
+  },
+  {
+    "revision": "410a8592f1a7fc6225a03deba78d94ff",
+    "url": "/Transylvenia/static/media/room-9.410a8592.jpg"
+  },
+  {
+    "revision": "5e1d4a0df2045d90a8d7e177f5d3fb88",
+    "url": "/Transylvenia/static/media/room-presi.5e1d4a0d.jpg"
+  }
+]);
